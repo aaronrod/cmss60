58c18d73b90b312d7892b94fe2e999a616e8b214
aaronrod <arodriguez@aereo.com>
2014-09-17 00:52:22 -0400

    adds SharedPref functionality via AsyncTask

--- app/src/main/java/android/cmss60/lesson1_youtube/Lesson1Activity.java
+++ app/src/main/java/android/cmss60/lesson1_youtube/Lesson1Activity.java
@@ -6,10 +6,11 @@ package android.cmss60.lesson1_youtube;
  * Objectives:
  * 1. Create a button
  * 2. Intents to other apps
- * 2. Manifest File
+ * 3. Manifest File
  */
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -25,11 +26,11 @@ import android.widget.Toast;
 public class Lesson1Activity extends Activity {
     private static final String YOUTUBE_MIT_URL = "http://youtu.be/AOokGMre4AM";
     private static final String YOUTUBE_CALTECH_URL = "http://youtu.be/zFH_haNX38E";
-    private static final String YOUTUBE_DEFAULT_URL = "http://youtu.be/8lXdyD2Yzls";
+    public static final String DEFAULT_URL = "http://youtu.be/8lXdyD2Yzls";
     private static final String TAG = "Lesson1Activity";
 
-    RadioButton mitRadioButton;
-    RadioButton caltechRadioButton;
+    private RadioButton mitRadioButton;
+    private RadioButton caltechRadioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +40,72 @@ public class Lesson1Activity extends Activity {
 
         mitRadioButton = (RadioButton)findViewById(R.id.mit_radioButton0);
         caltechRadioButton = (RadioButton)findViewById(R.id.caltech_radioButton1);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        //check to see if buttons have been saved previously
+        toggleState(false, null);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        //save the current state of the RadioButtons
+        toggleState(true, getUrl());
+    }
+
+    /**
+     * Uses an AsyncTask class for read/write from/to SharedPreferences.
+     * @param isInput may be null
+     * @param urlToSave
+     */
+    private void toggleState(final boolean isInput, final String urlToSave){
+        new SetPrefAsyncTask().execute(new VideoPreference() {
+            @Override
+            public Context getContext() {
+                return Lesson1Activity.this;
+            }
+
+            @Override
+            public boolean isInput() {
+                return isInput;
+            }
+
+            @Override
+            public String getKey() {
+                return "key_url";
+            }
+
+            @Override
+            public String getUrl() {
+                return urlToSave;
+            }
+
+            @Override
+            public String getFileName() {
+                return "video";
+            }
+
+            @Override
+            public void postExecute(String url) {
+                if(null == url) return;
+
+                //toggle RadioButtons. Checks for null in case the Activity has been removed
+                if(url.compareTo(YOUTUBE_MIT_URL) == 0 && mitRadioButton != null) {
+                    mitRadioButton.setChecked(true);
+                    return;
+                }
+
+                if(url.compareTo(YOUTUBE_CALTECH_URL) == 0 && caltechRadioButton != null){
+                    caltechRadioButton.setChecked(true);
+                    return; // here just in case you add more options later
+                }
+
+                //else do nothing for default
+            }
+        });
     }
 
     public void clickYouTube(View v){
@@ -57,7 +124,7 @@ public class Lesson1Activity extends Activity {
 
         if(caltechRadioButton.isChecked()) return YOUTUBE_CALTECH_URL;
 
-        return YOUTUBE_DEFAULT_URL;
+        return DEFAULT_URL;
     }
 
     @Override
