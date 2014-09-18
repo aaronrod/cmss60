ea61cf14a2868575c2cd57260d96817abe1316ac
aaronrod <arodriguez@aereo.com>
2014-09-18 15:50:50 -0400

    removes dependency of demo package

---
+++ app/src/main/java/android/cmss60/lesson2_browser/WebBrowser.java
@@ -0,0 +1,119 @@
+package android.cmss60.lesson2_browser;
+
+import android.app.Activity;
+import android.cmss60.core.SocialTVApplication;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class WebBrowser extends WebView {
+
+    protected static final String TAG = "WebBrowser";
+    protected static final String CNN_PAGE = "http://www.cnn.com";
+    protected static final String CNN_WORLD_PAGE = "http://www.cnn.com/WORLD/";
+    protected static final String HLS_TEST_PAGE = "http://www.jwplayer.com/html5/hls";
+
+    private final Activity mActivity;
+    private boolean mIsInitialized = false;
+    private Handler mOnInitHdlr;
+    int mHandlerWhat;
+    private ProgressBar mProgressBarSpinner;
+
+    public WebBrowser(Context context) {
+        super(context);
+        Log.v(TAG, "WebBrowser()");
+        mActivity = (Activity) context;
+    }
+
+    public WebBrowser(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        Log.v(TAG, "WebBrowser()");
+        mActivity = (Activity) context;
+    }
+
+    public WebBrowser(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        Log.v(TAG, "WebBrowser()");
+        mActivity = (Activity) context;
+    }
+
+    public void init(Handler handler, int handleWhat) {
+        Log.v(TAG, "init()");
+
+        if(mProgressBarSpinner != null){
+            mProgressBarSpinner.setVisibility(View.VISIBLE);
+        }
+
+        // set callback handler
+        mOnInitHdlr = handler;
+        mHandlerWhat = handleWhat;
+        // config WebView
+        configWebView();
+        // to listen to onPageFinished
+        setWebViewClient(new WebBrowserWebViewClient());
+        // load first/home page
+        // can't call from Java->JS until a page is loaded and we reach onPageFinished()
+        loadUrl(HLS_TEST_PAGE);
+        //loadUrl(CNN_PAGE);
+    }
+
+    public void setProgressBarSpinner(ProgressBar progressBar){
+        this.mProgressBarSpinner = progressBar;
+    }
+
+    protected class WebBrowserWebViewClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            Log.v(TAG, String.format("onPageFinished() url: %s", url));
+            if (!mIsInitialized) {
+                // first web page fully loaded
+                mIsInitialized = true;
+                mOnInitHdlr.sendEmptyMessage(mHandlerWhat);
+            }
+
+            if(mProgressBarSpinner != null){
+                mProgressBarSpinner.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.v(TAG, String.format("shouldOverrideUrlLoading() url: %s", url));
+            // If URL is CNN_WORLD_PAGE, load the web page in the Chrome browser
+            if (url.contains(CNN_WORLD_PAGE)) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                mActivity.startActivity(intent);
+                return true;
+            }
+            // load the web page in this WebView
+            return false;
+        }
+    }
+
+    private void configWebView() {
+
+        WebSettings settings = getSettings();
+
+        settings.setBuiltInZoomControls(true);
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setSaveFormData(true);
+        // enable JavaScript
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        // append the app specific info to the WebView user agent string
+        final String userAgent = settings.getUserAgentString() + " "
+                + ((SocialTVApplication) mActivity.getApplication()).getAppUserAgentString();
+        settings.setUserAgentString(userAgent);
+    }
+
+}
